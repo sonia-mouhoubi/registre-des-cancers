94809407b76093bda962041bc17ed1c2056af563
M <sonia.mouhoubi@laplateforme.io>
2022-07-12 11:27:46 +0200

    Connexion à la base de donnée

---
+++ models/Db.class.php
@@ -0,0 +1,23 @@
+<?php
+
+abstract class Db {
+    protected $db;
+
+    public function __construct() {
+        try {
+            $this->db = new PDO('mysql:host=localhost;dbname=registre_cancers;charset=utf8', 'root', '');
+
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // echo 'Connexion réussie';
+        }
+        catch (PDOException $e)
+        {
+            die('Erreur : ' . $e->getMessage());
+        } 
+        
+        // // Fermeture de la connexion
+        // $this->db = null;
+    }  
+}
+?>
+
