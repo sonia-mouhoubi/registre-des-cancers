17599218701b97ab4dbebe3e0cd5de07b93c400c
M <sonia.mouhoubi@laplateforme.io>
2022-07-14 23:55:11 +0200

    Création d'un rooter

--- index.php
+++ index.php
@@ -1,35 +1,13 @@
 <?php
+require_once('models/Rooter.class.php');
+
 if (isset($_GET['url'])) {
-
-    $name_file = explode("/",$_GET['url']);
-    $name_file = end($name_file);
-
+    $nameFile = explode("/",$_GET['url']);
+    $nameFile = end($nameFile);    
 } 
 else {
-
-    $name_file = "accueil";
-
+    $nameFile = "accueil";
 }
 
-function render($name_file) {
-    
-    $front_pages = ['accueil', 'actualites', 'chiffres-cancers', 'connexion', 'contact', 'activites-recherches', 'presentation', 'actualite-unique'];
-
-    if(in_array($name_file, $front_pages)) {
-
-        require("views/front-end/views-$name_file.php");
-
-    } else {
-
-        require("views/front-end/views-error404.php");
-
-    }
-}
-
-render($name_file);
-
-
-    
-    
-
-
+$routes = new Rooter;
+$routes->routes($nameFile);
---
+++ models/Rooter.class.php
@@ -0,0 +1,54 @@
+<?php
+
+class Rooter {
+    private $nameFile;
+
+    public function routes($nameFile) {
+
+        // Pages front
+    
+        $dirFront = 'views/front-end/'; // Chemin où se trouve le dossier.
+    
+        $arrayFilesFront = scandir($dirFront); // Permet de lire le dossier sous forme de tableau.
+    
+        $newArrayFront = []; // Array qui va contenir les noms de fichiers modifiés.
+    
+        foreach($arrayFilesFront as $key=> $value) {
+            if($key != 0 && $key != 1){
+                $file = substr($value,6, -4);
+                array_push($newArrayFront, $file);
+            }
+        }
+
+        // Pages back
+
+        $dirBack = 'views/back-end/'; // Chemin où se trouve le dossier.
+    
+        $arrayFilesBack = scandir($dirBack); // Permet de lire le dossier sous forme de tableau.
+    
+        $newArrayBack = []; // Array qui va contenir les noms de fichiers modifiés.
+    
+        foreach($arrayFilesBack as $key=> $value) {
+            if($key != 0 && $key != 1){
+                $file = substr($value,0, -4);
+                array_push($newArrayBack, $file);
+            }
+        }
+
+        
+        if(in_array($nameFile, $newArrayFront)) {
+    
+            require("views/front-end/views-$nameFile.php");
+        } 
+        elseif(in_array($nameFile, $newArrayBack)) {
+    
+            require("views/back-end/$nameFile.php");
+        }
+        else {
+    
+            require("views/front-end/views-error404.php");
+        }
+    }
+}
+
+?>
